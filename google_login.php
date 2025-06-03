96531947aeb4c2cceaf6fa8d62fcb530b3f2ed8a
yuan <a0492651586@gmail.com>
2025-06-03 16:59:28 +0800

    Secret

--- google_login.php
+++ google_login.php
@@ -2,8 +2,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setClientId('Client ID');
-$client->setClientSecret('Client Secret');
+$client->setClientId('1030569826848-1ltqho0j6ftthk0aj3f6eu5237s8jc2b.apps.googleusercontent.com');
+$client->setClientSecret('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');
 $client->setRedirectUri('https://google-login-dw05.onrender.com/google_callback.php');
 $client->addScope('email');
 $client->addScope('profile');
